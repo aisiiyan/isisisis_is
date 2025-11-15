1d131887b043f510bc02e7d6d4690cbdd0716908
aisiiyan <134267407+aisiiyan@users.noreply.github.com>
2025-11-15 09:10:40 +0700

    countries.js

--- countries.js
+++ countries.js
@@ -10,4 +10,3 @@ const countries = ["afghanistan","albania","algeria","andorra","angola","antigua
       "qatar","romania","russia","rwanda","saint kitts and nevis","saint lucia","saint vincent and the grenadines","samoa","san marino","sao tome and principe","saudi arabia","senegal","serbia","seychelles","sierra leone","singapore","slovakia","slovenia","solomon islands","somalia","south africa","spain","sri lanka","sudan","suriname","sweden","switzerland","syria",
       "taiwan","tajikistan","tanzania","thailand","timor-leste","togo","tonga","trinidad and tobago","tunisia","turkey","turkmenistan","tuvalu",
       "uganda","ukraine","united arab emirates","united kingdom","united states","uruguay","uzbekistan","vanuatu","vatican city","venezuela","vietnam","yemen","zambia","zimbabwe","antigua and barbuda","barbardos"];
-
